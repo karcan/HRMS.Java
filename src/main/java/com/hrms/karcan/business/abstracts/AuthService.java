f2d792fb7122cd26e41d938adfb7b04e3deedc29
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-19 23:30:29 +0300

    returned to first commit

--- src/main/java/com/hrms/karcan/business/abstracts/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.hrms.karcan.business.abstracts;
-
-import com.hrms.karcan.core.utilities.result.Result;
-
-public interface AuthService {
-	Result login(String emailAdress, String password);
-}
--- src/main/java/com/hrms/karcan/business/abstracts/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.hrms.karcan.business.abstracts;
-
-import com.hrms.karcan.entity.concretes.User;
-
-public interface UserService {
-	void save(User user);
-	User findByEmailAddressAndPassword(String email, String password);
-}
