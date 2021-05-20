9ee9ea876fdd5e536c1e5fa16ca9b45a6ed7da99
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-05-20 16:59:43 +0300

    .

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/karcan-0.0.1-SNAPSHOT.jar 
--- src/main/java/com/hrms/karcan/entity/dtos/UserLoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.hrms.karcan.entity.dtos;
-
-import lombok.Data;
-
-@Data
-public class UserLoginDto {
-	private String email;
-	private String password;
-}
