790fe42e483fddcaaafecc85e766f6be3c8990dc
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2021-06-06 15:37:02 +0300

    re-initialize

--- src/test/java/com/hrms/karcan/KarcanApplicationTests.java
+++ src/test/java/com/hrms/karcan/KarcanApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class KarcanApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
